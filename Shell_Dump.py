87adf10a54a0935fad397433dc74b8f3c31cdf63
jac11 <49693666+jac11@users.noreply.github.com>
2020-04-14 19:47:33 -0400

    Update Shell_Dump.py

--- Shell_Dump.py
+++ Shell_Dump.py
@@ -4,14 +4,17 @@ import readline
 import re
 import os
 import time 
-import shutil 
-
+import shutil
+import subprocess  
+  
 class Shell_Dump:
       def __init__(self):
-       
-        self.Banner()     
+        
+        self.Banner()
         self.Create_file()
         self.file_path()
+        self.IF_OPtion()
+        self.LINKER_ASSEMBLY()
         self.replace()
         
       def Banner(self):
@@ -26,7 +29,6 @@ class Shell_Dump:
                                                          |_|   
                       """ 
          print self.Banner
-  
       def Create_file(self):
          try: 
              try:    
@@ -41,26 +43,23 @@ class Shell_Dump:
              except OSError:
                  time.sleep(2)
                  os.chdir(self.dir_1)
-                 print "\n[*]File is Ready Exits [*]",os.getcwd()
+                 print "\n[*]File is Ready Exist[*]",os.getcwd()
                  time.sleep(2)
          except KeyboardInterrupt:
               print self.Banner
               exit() 
-              
       def file_path(self):
           try:
             try:   
                 destntion = os.getcwd()
-                print"\n[$]Please Copy the Output objdump.o To 'Plan.txt' "
+                print
+                self.file_cname = str(raw_input("\n[X]Enter The File Name Example.asm : ")).strip()
                 time.sleep(2)
-                self.file_cname= str(raw_input("\n[X]Enter The File Name Of The 'Plan.txt': ")).strip()
-                time.sleep(2)
-                self.path_copy = str(raw_input("\n[E]Please Enter The File Path Of :")).strip()
-                self.fpath=shutil.copy(self.path_copy+self.file_cname,"./")
-                time.sleep(2)
-                print "\n[%]Please Enter File Name To Save The ShellCode Resulet[$] "
-                time.sleep(2)
-                self.file_name= str(raw_input("\n[E]Please Enter The File Name :")).strip()
+                self.path_copy = str(raw_input("\n[E]Please Enter The File Path Example.asm :")).strip()
+                self.fpath = shutil.copy(self.path_copy+self.file_cname,"./")
+                self.file1=str(self.file_cname + "_obj")
+                self.file2=str(self.file_cname + "_dump")
+                self.file3=str(self.file_cname + "_shellcode")
             except IOError :
                  time.sleep(2)
                  print"\n{O}SOMETHING IS WROING PLSEASE CHECK YOU FILE PATH{O}"
@@ -68,10 +67,58 @@ class Shell_Dump:
                  self.file_path()        
           except KeyboardInterrupt:
               print self.Banner
-              exit()           
+              exit()   
+      def IF_OPtion(self):
+           try:              
+                 opt_1 = "x86"
+                 opt_2 = "x64"
+                 time.sleep(2)
+                 SELECT = str(raw_input("\n[@]Please Selcet x84 or x64 : "))
+                 print
+                 if SELECT == opt_1 and  len(SELECT) == 3 : 
+                       code = subprocess.call(['nasm','-f','elf32',"{}".format(self.file_cname),'-o','{}'.format(self.file1)])
+                       if code == 1 :
+                           print
+                           print " [!] Instruction Not Completed Not Supported in 86-bit Mode [!]"
+                           time.sleep(2)
+                           return self.IF_OPtion()
+                       else:
+                          time.sleep(2)
+                          print "\ntThe Object File x86 is Generated  !! "         
+                                                                                                         
+                 elif SELECT == opt_2 and len(SELECT)==3:                  
+                       code = subprocess.call(['nasm','-f','elf64',"{}".format(self.file_cname),'-o','{}'.format(self.file1)])                     
+                       if code == 1 :
+                           print
+                           print " [!] Instruction Not Completed Not Supported in 64-bit Mode [!]"
+                           time.sleep(2)
+                           return self.IF_OPtion()
+                       else:
+                          print "\nThe Object File x64 is Generated !! " 
+                          time.sleep(2)  
+                 else :
+                       print " [!] Instruction Not Completed Not Supported [!]"
+                       time.sleep(2)
+                       return self.IF_OPtion()     
+                        
+           except KeyboardInterrupt:
+              print self.Banner
+              exit()  
+                         
+      def LINKER_ASSEMBLY(self):
+          try:
+             time.sleep(2)        
+             print "\n[W]The Linker Process Started [W]\n "
+             time.sleep(2)
+             subprocess.call(['ld','-n','-o','txt','{}'.format(self.file1)])
+             with open(self.file2,'w')as file:                 
+                stdout =  subprocess.call(['objdump','-d','{}'.format(self.file1)], stdout=file, stderr=file)   
+          except KeyboardInterrupt:
+              print self.Banner            
+              exit()                    
       def replace(self):
           try:
-              with open(self.file_cname,"r") as file:
+              with open(self.file2,"r") as file:
                         read = file.read()  
               findall= str(re.findall(":....................\D",read))
            
@@ -89,11 +136,12 @@ class Shell_Dump:
               Header= "".join("\\x%s"%Header[i:i+2] for i in range(0, len(Header), 2))
               Header= "".join('\n"%s"'%Header[i:i+56] for i in range(0, len(Header),56))
               
-              with open(self.file_name,'wb')as file: 
+              with open(self.file3,'wb')as file: 
                      file.write(Header)  
-              with open (self.file_name,'rb')as test:
+              with open (self.file3,'rb')as test:
                       test= test.read()
                       time.sleep(2)
+                      print
                       print "\t\t\t\t\t\t{%}THE SHELLCODE{%}"
                       print "\t\t\t\t\t[@]++++++++++++++++++++++++++[@]\n"
                       time.sleep(2)
@@ -104,6 +152,8 @@ class Shell_Dump:
               exit
           except KeyboardInterrupt:
               print self.Banner            
-              exit()                    
-if __name__=="__main__":
-         Shell_Dump()     
+              exit()                  
+               
+if __name__=="__main__": 
+       Shell_Dump()
+        
